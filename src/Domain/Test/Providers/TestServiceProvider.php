f93978e0a47a9ebddc8275a73ffcae00f2859901
Peter Elmered <peter@elmered.com>
2024-04-10 09:34:28 +0200

    Setup DDD and create a first Test domain

---
+++ src/Domain/Test/Providers/TestServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Domain\Test\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TestServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
